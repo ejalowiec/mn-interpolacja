6c1ccdc8f0640e6b3d18942ac51415a44abdcb49
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2020-11-10 21:16:57 +0100

    added saving output to the file

--- interpolacja.cpp
+++ interpolacja.cpp
@@ -1,10 +1,24 @@
 ﻿#include <iostream>
-
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+string imie, nazwisko;
+int nr_tel;
 
 int main() {
+	cout << "Imie: "; cin >> imie;
+	cout << "Nazwisko: "; cin >> nazwisko;
+	cout << "Nr tel: "; cin >> nr_tel;
+
+	fstream file;
+	file.open("test.txt", ios::out);
+	file << imie << endl;
+	file << nazwisko << endl;
+	file << nr_tel << endl;
+
+	file.close();
 
 	return 0;
 }
