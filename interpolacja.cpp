63f20c824cb4cbd02cb6a86fe7afde70ce07bf7c
Eryk Jalowiec <eryk.jalowiec@gmail.com>
2020-11-11 10:07:51 +0100

    added reading from file as it should be originally

--- interpolacja.cpp
+++ interpolacja.cpp
@@ -11,30 +11,39 @@ int nr_tel;
 int main() {
 
 	fstream file;
-	file.open("test.txt", ios::in);
+	file.open("plik2.txt", ios::in);
 
 	if (file.good() == false) {
-		cout << "File does not exist";
+		cerr << "File does not exist";
 		exit(0);
 	}
 
-	string line;
-	int noofline = 1;
+	int size;
+	file >> size;
 
-	while (getline(file, line)) {
-		switch (noofline) {
-		case 1: imie = line; break;
-		case 2: nazwisko = line; break;
-		case 3: nr_tel = atoi(line.c_str()); break;
-		}
-		noofline++;
+	int** arr = new int*[size];
+	for (int i = 0; i < size; ++i)
+		arr[i] = new int[2];
+
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < 2; j++)
+			file >> arr[i][j];
 	}
-
+	
 	file.close();
 
-	cout << imie << endl;
-	cout << nazwisko << endl;
-	cout << nr_tel << endl;
+	cout << size << endl;
+	cout << arr[0][0] << endl;
+	cout << arr[1][0] << endl;
+	cout << arr[2][0] << endl;
+	cout << arr[3][0] << endl;
+	cout << arr[4][0] << endl;
+	cout << arr[0][1] << endl;
+	cout << arr[1][1] << endl;
+	cout << arr[2][1] << endl;
+	cout << arr[3][1] << endl;
+	cout << arr[4][1] << endl;
+
 
 	return 0;
 }
